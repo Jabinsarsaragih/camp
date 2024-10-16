7ab1b68b6d5178b0460bf8961361188f25ec57ec
rasyid822 <krasyid822@gmail.com>
2024-10-16 08:14:48 +0700

    fix service worker

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,26 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/'
+];
+
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('v1').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/rsc/css/style.css',
-          'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
-          'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
-        ]);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
       })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       })
-    );
-  });
+  );
+});
